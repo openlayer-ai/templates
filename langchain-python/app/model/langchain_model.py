ea63bce7b9e07fdfcbb7e38981bdad2420774673
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2024-04-22 08:51:05 -0300

    Completes OPEN-5910 Create template GitHub repos [LangChain - Python]

---
+++ langchain-python/app/model/langchain_model.py
@@ -0,0 +1,30 @@
+"""Module for my LangChain model."""
+
+from typing import Dict, List
+
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from openlayer.integrations import langchain_callback
+
+
+class LangChainModel:
+    """Class for a LangChain model.
+
+    The class instantiates Openlayer's callback handler, which is used to enable
+    tracing calls to chat completion LLMs with LangChain. It also defines an `invoke`
+    method, which is used to call the model with a message and uses the callback
+    handler.
+
+    The `invoke` method is what's used throughout the app, to keep
+    a single source of truth for the model.
+    """
+
+    def __init__(self):
+        """Make sure you import / instantiate your system here."""
+        self.openlayer_handler = langchain_callback.OpenlayerHandler()
+
+    def invoke(self, message: str) -> str:
+        """Wrapper for the chain / model invokation."""
+        chat = ChatOpenAI(max_tokens=25, callbacks=[self.openlayer_handler])
+        response = chat.invoke([HumanMessage(content=message)])
+        return response.content
---
+++ langchain-python/app/server.py
@@ -0,0 +1,58 @@
+"""Sample Flask app using the RAG pipeline.
+
+The model is imported from the `model.rag_pipeline` module, which is where we wrapped
+the OpenAI client with Openlayer's Python SDK `OpenAIMonitor` and decorated the methods
+we wanted to trace with `@tracer.trace()`.
+
+This sample app is based on OpenAI's original Python example: https://github.com/openai/openai-quickstart-python.
+"""
+
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    jsonify,
+    render_template,
+    request,
+)
+from model.langchain_model import LangChainModel
+
+load_dotenv(dotenv_path=".env")
+
+
+model = LangChainModel()
+app = Flask(__name__)
+
+chat_history = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", chat_history=chat_history)
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    content = request.json["message"]
+    chat_history.append({"role": "user", "content": content})
+    return jsonify(success=True)
+
+
+@app.route("/query", methods=["POST"])
+def query():
+    user_query = request.json["message"]
+    assistant_response = model.invoke(user_query)
+    chat_history.append({"role": "assistant", "content": assistant_response})
+    return jsonify(success=True, message=assistant_response)
+
+
+@app.route("/reset", methods=["POST"])
+def reset_chat():
+    global chat_history
+    chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
+    return jsonify(success=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
