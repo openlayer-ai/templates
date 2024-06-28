dabebcc1348f58774e7908caeb451696c53e9506
Rishab Ramanathan <rishramanathan@gmail.com>
2024-06-28 09:35:15 -0700

    feat: add ragas metric as a custom metric

--- python/llms/openai-chatbot/metrics/run_all.py
+++ python/llms/openai-chatbot/metrics/run_all.py
@@ -2,8 +2,9 @@
 
 # Import all your custom metrics here
 from my_metric import RishabMetric
+from answer_correctness import Correctness
 from openlayer.lib.core import metrics
 
 
 if __name__ == "__main__":
-    metrics.MetricRunner().run_metrics([RishabMetric()])
+    metrics.MetricRunner().run_metrics([RishabMetric(), Correctness()])
